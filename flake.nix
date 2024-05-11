c388efbc02864939c0bb8311cc222efb1c9c3d05
diogo antunes <diogo.santiago.pt@gmail.com>
2024-05-11 19:16:38 +0100

    feat: added nix flake

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "CPS Project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-23.05";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = import nixpkgs { system = "x86_64-linux"; };
+  in
+  {
+    devShell.x86_64-linux = pkgs.mkShell {
+      buildInputs = with pkgs; [ rustc cargo gcc rustfmt clippy ];
+    };
+  };
+}
